1e4d0c2b6a7d5ff4d9c459b8eab9befb57218156
Kevyn02 <kaparecido483@gmail.com>
2022-12-18 17:29:40 -0300

    Aula 2

--- src/infra/app.controller.ts
+++ src/infra/app.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { randomUUID } from 'crypto';
-import { CreateNotificationBody } from './create-notification-body';
-import { PrismaService } from './prisma.service';
+import { CreateNotificationBody } from './http/dtos/create-notification-body';
+import { PrismaService } from './database/prisma/prisma.service';
 
 @Controller('notifications')
 export class AppController {
--- src/infra/http/dtos/create-notification-body.ts
+++ src/infra/http/dtos/create-notification-body.ts
@@ -2,7 +2,6 @@ import { IsNotEmpty, IsUUID, Length } from 'class-validator';
 
 export class CreateNotificationBody {
   @IsNotEmpty()
-  @IsUUID()
   content: string;
 
   @IsNotEmpty()
@@ -10,5 +9,6 @@ export class CreateNotificationBody {
   category: string;
 
   @IsNotEmpty()
+  @IsUUID()
   recipientId: string;
 }
